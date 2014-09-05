b2dd1273357433df82020ea1009b667c329dab6a
Alex S <bobylito@gmail.com>
2014-09-06 00:57:40 +0200

    Add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    browserify: {
+      options: {
+        transform: [ require('grunt-react').browserify ],
+        bundleOptions:{
+          debug: true
+        }
+      },
+      app: {
+        src : 'src/js/app.js',
+        dest: 'dist/js/app.js'
+      }
+    },
+    stylus : {
+      dev:{
+        options:{
+          linenos : true
+        },
+        files : {
+          './dist/css/app.css' : './src/stylus/index.styl'
+        }
+      }
+    },
+    watch: {
+      files: ['./src/**/*'],
+      tasks: ['build']
+    },
+  })
+
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+
+  grunt.registerTask('default', ['build', 'watch']);
+  grunt.registerTask('build', ['browserify:app', 'stylus']);
+  //grunt.registerTask('serve', ['connect:server']);
+};
