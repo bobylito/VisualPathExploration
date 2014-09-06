a3279315eedf60c1185f5957a93e7379781f5fc8
Alex S <bobylito@gmail.com>
2014-09-06 13:20:50 +0200

    Bigger drawing space

--- src/js/app.js
+++ src/js/app.js
@@ -3,4 +3,4 @@
 var React = require("react/addons");
 var Main = require("./components");
 
-React.renderComponent( <Main file="data/train.csv" height="600" width="800"/>, window.document.body );
+React.renderComponent( <Main file="data/train.csv" height="800" width="800"/>, window.document.body );
